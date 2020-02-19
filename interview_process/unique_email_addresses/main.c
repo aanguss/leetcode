b13ee98fa90b4d7d4561672a01f6715594a21152
Tony <aangus@gmail.com>
2020-02-19 00:26:21 -0800

    added interview process question 1 - unique email addresses

---
+++ interview_process/unique_email_addresses/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int numUniqueEmails(char ** emails, int emailsSize){
+    int numEmailsSent = 0;
+    char *emailList[emailsSize];
+    for (int i = 0; i < emailsSize; i++){
+        printf("blah #%d\n", i);
+        printf("%s\n", emails[i]);
+        printf("size = %d", sizeof(emails[i])/sizeof(emails[0]));
+        // for(int j = 0; j < sizeof(emails[i])/sizeof(char*); j++) {
+        //     printf("%c", *emails+j);
+        // }
+        printf("\n\n");
+    }
+}
+
+int main(void) {
+    char *emailList[3] = {   
+        "test.email+alex@leetcode.com",
+        "test.e.mail+bob.cathy@leetcode.com",
+        "testemail+david@lee.tcode.com"};
+    // char ** pEmailList = *emailList;
+    int numOfEmails = numUniqueEmails(emailList,
+            sizeof(emailList)/sizeof(char*));
+    // printf("%d", emailList[0].length);
+    printf("%d", numOfEmails);
+
+    return 0;
+}
