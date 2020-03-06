f55f228f80beddfedf4239aaea2a134d03f6b895
Anthony Angus <anthony.angus@intel.com>
2020-03-06 13:22:57 -0800

    fix for O(logn) on first_and_last_index_of_sorted_array leetcode challenge

--- leetcode/5_sorting_and_searching/first_and_last_index_of_sorted_array.c
+++ leetcode/5_sorting_and_searching/first_and_last_index_of_sorted_array.c
@@ -23,7 +23,7 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int* searchRange(int* nums, int numsSize, int target, int* returnSize) {
+int* searchRange_brute(int* nums, int numsSize, int target, int* returnSize) {
     int indexStart = -1;
     int indexEnd = -1;
 
@@ -46,6 +46,73 @@ int* searchRange(int* nums, int numsSize, int target, int* returnSize) {
         
     }
     return returnArray;
+}
+
+int findLeftIndex(int* nums, int target, int leftIndex, int rightIndex) {
+    int middle = (leftIndex + rightIndex) / 2;
+
+    if (middle == 0 && nums[middle] != target) {
+        return -1;
+    } else {
+        if ((nums[middle-1] != target) && (nums[middle] == target)) {
+            return middle;
+        }
+        else {
+            if (nums[middle] <= target) {
+                middle = findLeftIndex(nums, target, middle + 1, rightIndex);
+            } else {
+                middle = findLeftIndex(nums, target, leftIndex, middle);
+            }
+        }
+    }
+    return middle;
+}
+
+int findRightIndex(int* nums, int target, int leftIndex, int rightIndex) {
+    int middle = (leftIndex + rightIndex) / 2;
+
+    if (middle == rightIndex && nums[middle] != target) {
+        return middle;
+    } else {
+        if ((nums[middle+1] != target) && (nums[middle] == target)) {
+            return middle;
+        }
+        else {
+            if (nums[middle] <= target) {
+                middle = findRightIndex(nums, target, middle + 1, rightIndex);
+            } else {
+                middle = findRightIndex(nums, target, leftIndex, middle);
+            }
+        }
+    }
+    return middle;
+}
+
+int* searchRange(int* nums, int numsSize, int target, int* returnSize) {
+    int indexStart = -1;
+    int indexEnd = -1;
+    int middleIndex = numsSize / 2;
+    int leftRange = 0, rightRange = numsSize;
+    
+    indexStart = findLeftIndex(nums, target, 0, middleIndex);
+    indexEnd = findRightIndex(nums, target, middleIndex + 1, numsSize);
+
+    int * returnArray = malloc(sizeof(int) * *returnSize);
+    returnArray[0] = indexStart;
+    returnArray[1] = indexEnd;
+
+    return returnArray;
+
+
+    // int middleIndex = numsSize / 2;
+
+    // if (middleIndex == target) {
+    //     indexStart = middleIndex;
+    // }
+
+    // if (target < middleIndex) {
+
+    // }
 }
 
 int main(void) {
