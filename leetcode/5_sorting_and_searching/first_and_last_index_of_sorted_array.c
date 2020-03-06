cdbfde5ff6aee524086bfc510b83c43b8c66ef68
Tony <aangus@gmail.com>
2020-03-05 17:55:46 -0800

    added 2x sorting and search problems

---
+++ leetcode/5_sorting_and_searching/first_and_last_index_of_sorted_array.c
@@ -0,0 +1,30 @@
+/* https://leetcode.com/explore/interview/card/google/63/sorting-and-searching-4/3081/ */
+/**
+ * Given an array of integers nums sorted in ascending order, find the
+ * starting and ending position of a given target value.
+ * 
+ * Your algorithm's runtime complexity must be in the order of O(log n).
+ * 
+ * If the target is not found in the array, return [-1, -1].
+ */
+/**
+ * Example #1
+ * Input: nums = [5,7,7,8,8,10], target = 8
+ * Output: [3,4]
+ */
+/**
+ * Example #2
+ * Input: nums = [5,7,7,8,8,10], target = 6
+ * Output: [-1,-1]
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* searchRange(int* nums, int numsSize, int target, int* returnSize){
+
+}
