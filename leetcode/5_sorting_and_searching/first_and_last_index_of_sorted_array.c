8c4ee82c9b9d043affd7066f0780aabcd7b544f7
Tony <aangus@gmail.com>
2020-03-05 23:30:21 -0800

    brute force of first_and_last_index_of_sorted_array

--- leetcode/5_sorting_and_searching/first_and_last_index_of_sorted_array.c
+++ leetcode/5_sorting_and_searching/first_and_last_index_of_sorted_array.c
@@ -20,11 +20,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int* searchRange(int* nums, int numsSize, int target, int* returnSize){
+int* searchRange(int* nums, int numsSize, int target, int* returnSize) {
+    int indexStart = -1;
+    int indexEnd = -1;
 
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] == target && indexStart == -1) {
+            indexStart = i;
+        }
+        if (nums[i] == target && indexStart != -1) {
+            indexEnd = i;
+        }
+    }
+    int * returnArray = malloc(sizeof(int) * *returnSize);
+    returnArray[0] = indexStart;
+    if (indexStart != -1) {
+        if (indexEnd != -1) {
+            returnArray[1] = indexEnd;
+        } else {
+            returnArray[1] = indexStart;
+        }
+        
+    }
+    return returnArray;
+}
+
+int main(void) {
+    int arrayOne[6] = {5,7,7,8,8,10};
+    int targetOne = 8;
+    int returnSize = 2;
+    int * arrayReturn = malloc(sizeof(int) * returnSize);
+    arrayReturn = searchRange(arrayOne, sizeof(arrayOne)/sizeof(arrayOne[0]), targetOne, &returnSize);
+    // int* searchRange(int* nums, int numsSize, int target, int* returnSize) 
+    for (int i = 0; i < returnSize; i++) {
+        printf("array[%d] = %d\n", i, arrayReturn[i]);
+    }
+    free(arrayReturn);
+    return 0;
 }
