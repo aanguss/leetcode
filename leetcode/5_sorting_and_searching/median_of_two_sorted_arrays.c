89a4fbe9f45521b5795c50df839f5aad1a41164e
Tony <aangus@gmail.com>
2020-03-05 21:26:57 -0800

    O(n) method... doesn't pass all tests and isn't O(log(m+n))...

--- leetcode/5_sorting_and_searching/median_of_two_sorted_arrays.c
+++ leetcode/5_sorting_and_searching/median_of_two_sorted_arrays.c
@@ -25,23 +25,32 @@
 
 double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size){
     double median1 = 0, median2 = 0;
+
     for(int i = 0; i < nums1Size; i++) {
         median1 += nums1[i];
     }
-    for(int i = 0; i < nums1Size; i++) {
+    for(int i = 0; i < nums2Size; i++) {
+        // printf("-->found nums2[%d] = %d\n", i, nums2[i]);
         median2 += nums2[i];
     }
-    median1 = median1 / nums1Size;
-    median2 = median2 / nums2Size;
-    return (median1 + median2) / 2;
+       
+    return ((median1 + median2) / (nums1Size + nums2Size));
 }
 
 int main(void) {
     int nums1[2] = {1, 2};
     int nums2[2] = {3, 4};
-    double median;
+    double median1;
     
-    median = findMedianSortedArrays(nums1, sizeof(nums1)/sizeof(nums1[0]),
-            nums2, sizeof(nums2)/sizeof(nums2[0]));
-    printf("media = %f\n", median);
+    median1 = findMedianSortedArrays(   nums1, sizeof(nums1)/sizeof(nums1[0]),
+                                        nums2, sizeof(nums2)/sizeof(nums2[0]));
+    printf("median1 = %f\n", median1);
+
+    int nums3[2] = {1, 3};
+    int nums4[1] = {2};
+    double median2;
+    
+    median2 = findMedianSortedArrays(   nums3, sizeof(nums3)/sizeof(nums3[0]),
+                                        nums4, sizeof(nums4)/sizeof(nums4[0]));
+    printf("media2 = %f\n", median2);
 }
