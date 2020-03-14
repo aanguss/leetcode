eb4e2bd2b7c387010e25de20d5bdf30b247d128c
Anthony Angus <anthony.angus@intel.com>
2020-03-13 18:00:40 -0700

    add scott and ted

---
+++ friends/tedTalks/2020_03_13_k_closest_points.c
@@ -0,0 +1,36 @@
+/*** https://leetcode.com/problems/k-closest-points-to-origin/
+ */
+
+// #include <myPriorityQueue.h>
+
+
+int dequeue(i) {
+    // blah blah
+}
+
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int** kClosest(int** points, int pointsSize, int* pointsColSize, int K,         int* returnSize, int** returnColumnSizes){
+
+
+// 1. loop through array of points
+// 2. calculate the distances
+// 3. hashtable (points, distance)
+// 4. loop and find largest points return hash table values
+
+    for (int i = 0; i < pointsSize; i++) {
+
+    }
+ 
+
+}
+
+int main(void) {
+
+
+    return 0;
+}
