23958b6f856cf0ac57d44336b678c8557576272b
Anthony Angus <anthony.angus@intel.com>
2020-03-16 14:36:32 -0700

    additional questions from nicole

---
+++ friends/nicole/sweetness.c
@@ -0,0 +1,22 @@
+// why programming?
+
+// dropped into a new tool, what is your method of learning?
+// does the interviewer know how to learn
+
+// const, makes firmware code more debuggable (team function - variable shoudlnt' change)
+
+// example of a project - priorities and how they were identified
+
+// predefined range with array  and linked list is dynamic
+// linked list you have to go through whole list and array you can access index directly
+
+// cpu polling vs interrupt to just come in
+// interrupts can do edge and level
+// polling can only do level
+
+// debug memory - apply a known pattern in question
+
+// defrag algorithm
+// hardware limitation - dont want to cut performance - dont block host traffic
+// PLI - how to track before you lost power
+// defrag.c
