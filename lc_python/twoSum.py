6623bad079d878005e7ef9e01c72919501724c47
Anthony Angus <anthony.angus@intel.com>
2020-08-18 02:28:01 -0700

    numIslands messy but done

--- lc_python/twoSum.py
+++ lc_python/twoSum.py
@@ -2,7 +2,7 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        sumList = []
+        # sumList = []
 
         for a in range(len(nums)):
             for b in range(a + 1, len(nums)):
