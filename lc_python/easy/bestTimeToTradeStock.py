1733e3d3245ece39967ade3415ff9c8216872b5f
Tony <aangus@gmail.com>
2020-09-03 23:20:54 -0700

    update to bestTimeToTradeStock.py

--- lc_python/easy/bestTimeToTradeStock.py
+++ lc_python/easy/bestTimeToTradeStock.py
@@ -17,7 +17,7 @@
 from typing import List
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-#         This option takes too long - fails on 199/200
+#---------This option takes too long - fails on 199/200
 #         maxValue = 0
 #         minValue = 0xFFFFFFFF
 #         buyIndex = -1
@@ -35,31 +35,48 @@ class Solution:
 #                         maxValue = prices[j] - prices[i]
 #                         sellIndex = j
 #         return maxValue
+#
 
+#---------This is also slow - track only min and max with 1 pass instead
+#         maxValue = 0
+#         minValue = 0xFFFFFFFF
+#         buyIndex = -1
+#         sellIndex = -1
+#         buyPrice = 0xFFFFFFFF
+#         sellPrice = 0
+#         maxProfit = 0
+        
+#         if len(prices) <= 1:
+#             return 0
+        
+#         for i in range(len(prices) - 1, -1, -1):
+#             # print(prices[i])
+#             if prices[i] > sellPrice:
+#                 sellPrice = prices[i]
+#                 buyPrice = 0xFFFFFFFF
+#                 # print("sellPrice = %d" % sellPrice)
+#                 for j in range(i - 1, -1, -1):
+#                     if prices[j] < buyPrice:
+#                         buyPrice = prices[j]
+#                         # print("buyPrice = %d" % buyPrice)
+#                         if sellPrice - buyPrice > maxProfit:
+#                             maxProfit = sellPrice - buyPrice
+#                             # print("maxProfit = %d - %d = %d" % (sellPrice, buyPrice, maxProfit))
+#         return maxProfit
+#---------This is much quicker and has only one pass
         maxValue = 0
         minValue = 0xFFFFFFFF
-        buyIndex = -1
-        sellIndex = -1
-        buyPrice = 0xFFFFFFFF
-        sellPrice = 0
         maxProfit = 0
         
-        if len(prices) <= 1:
-            return 0
-        
-        for i in range(len(prices) - 1, -1, -1):
-            # print(prices[i])
-            if prices[i] > sellPrice:
-                sellPrice = prices[i]
-                buyPrice = 0xFFFFFFFF
-                # print("sellPrice = %d" % sellPrice)
-                for j in range(i - 1, -1, -1):
-                    if prices[j] < buyPrice:
-                        buyPrice = prices[j]
-                        # print("buyPrice = %d" % buyPrice)
-                        if sellPrice - buyPrice > maxProfit:
-                            maxProfit = sellPrice - buyPrice
-                            # print("maxProfit = %d - %d = %d" % (sellPrice, buyPrice, maxProfit))
+        for price in prices:
+            if price > maxValue: # maxValue is found so find maxProfit
+                maxValue = price
+                if maxValue - minValue > maxProfit:
+                    maxProfit = maxValue - minValue
+            if price < minValue: # minValue is found so need new max
+                minValue = price
+                maxValue = 0
+                
         return maxProfit
         
 s = Solution()
