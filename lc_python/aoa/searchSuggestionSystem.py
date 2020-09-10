3921642832ae0b9452c31cdf6971c545c6bfb082
aanguss <aangus@gmail.com>
2020-09-09 23:05:26 -0700

    submitted suggested products

--- lc_python/aoa/searchSuggestionSystem.py
+++ lc_python/aoa/searchSuggestionSystem.py
@@ -44,7 +44,7 @@
 #   All characters of searchWord are lower-case English letters.
 from typing import List
 class Solution:
-    def getSearchSuggestion(self, products: List[str], searchWord:str) -> List[List[str]]:
+    def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
         searchListPerChar = []
         for i in range(1, len(searchWord) + 1):
             # print(searchWord[:i])
@@ -68,5 +68,5 @@ solution =  [["mobile", "moneypot", "monitor"],
              ["mouse", "mousepad"],
              ["mouse", "mousepad"],
              ["mouse", "mousepad"]]
-output = s.getSearchSuggestion(products, searchWord)
+output = s.suggestedProducts(products, searchWord)
 print("%s | %s" % (output == solution, output))
