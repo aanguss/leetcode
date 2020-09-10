f427b4538ae9cbd7749a42534cff7c8ffa352299
aanguss <aangus@gmail.com>
2020-09-10 00:03:30 -0700

    fixed that stupid reorder log file problem

--- lc_python/easy/reorderLogFiles.py
+++ lc_python/easy/reorderLogFiles.py
@@ -8,7 +8,9 @@
 # Each word after the identifier will consist only of digits.
 # We will call these two varieties of logs letter-logs and digit-logs.  It is guaranteed that each log has at least one word after its identifier.
 #
-# Reorder the logs so that all of the letter-logs come before any digit-log.  The letter-logs are ordered lexicographically ignoring identifier, with the identifier used in case of ties.  The digit-logs should be put in their original order.
+# Reorder the logs so that all of the letter-logs come before any digit-log.  
+# The letter-logs are ordered lexicographically ignoring identifier, with the identifier used in case of ties.  
+# The digit-logs should be put in their original order.
 #
 # Return the final order of the logs.
 #
@@ -30,70 +32,49 @@ class Solution:
         
         for log in logs:
             logSplit = log.split()
-            print(logSplit[1])
-            if logSplit[1] in ['0','1','2','3','4','5','6','7','8','9']:
-                digitLogs.append(log)
+            # print(logSplit[0], logSplit[1])
+            if not logSplit[1].isdigit():
+                # print("found letter")
+                letterLogs.append(log.split())
             else:
-                letterLogs.append(log)
-        
-        # print(letterLogs)
-        # letterLogs.sort()
-        # print(letterLogs)
-        # digitLogs.sort()
-        
-        
-        
-        
-        letterLogsSorted = []
-        digitLogsSorted = []
+                # print("found digit")
+                digitLogs.append(log)
         
         if letterLogs:
-            # for log in letterLogs:
-            #     letterLogsSorted.append(log.split())
-                
-            # print(len(letterLogsSorted))
-            # print(letterLogsSorted[0])
-            
-            # for log in letterLogs:
-            #     print(log)
-            # print('\n')
-            letterLogs.sort(key=len)
-            
-            # print(len(letterLogsSorted[0]))
-            # for i in range(len(letterLogsSorted[0])):
-            # for i in range(len(letterLogsSorted)): 
-            # for j in range(max(len(x) for x in letterLogsSorted)):
-            #     if(letterLogsSorted[0][j]):
-            #         letterLogsSorted.sort(key = lambda letterLogsSorted: letterLogsSorted[0][j])
-            # print(letterLogsSorted[1][1])
-            
-            # letterLogsSorted.sort(key = lambda letterLogsSorted: letterLogsSorted[1][1])
-            # for log in letterLogs:
-            #     print(log)
-                
-            # for i in range(min(len(x) for x in letterLogs)):
-            #     letterLogs.sort(key = lambda letterLogs: letterLogs[i])
-            # for i in range(len(letterLogs[0])):
-            #     for j in range(len(letterLogs[0] + 1)):
-            #         if 
-            
-                    
+            # print(letterLogs)
+            letterLogsSorted = []
+            # first sort lexiwhateverish
+            letterLogs.sort()
+            # now sort with 2nd index and on
+            letterLogs.sort(key = lambda x: x[1:])
             for log in letterLogs:
-                newLogs.append(log)
-        if digitLogs:
-            # print(len(digitLogs[0]))
-            # print(digitLogs[0])
-            # for i in range(min(len(x) for x in digitLogs)):
-            #     digitLogs.sort(key = lambda digitLogs: digitLogs[i])
-            # digitLogs.sort(key=len)
-            for log in digitLogs:
-                newLogs.append(log)
+                log = " ".join(log)
+                letterLogsSorted.append(log)
+            # print(letterLogsSorted)
             
+            newLogs.extend(letterLogsSorted)
+
+        if digitLogs:
+            newLogs.extend(digitLogs)
+
         return newLogs
 
 s = Solution()
 logs = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"]
 answer = ["let1 art can","let3 art zero","let2 own kit dig","dig1 8 1 5 1","dig2 3 6"]
+output = s.reorderLogFiles(logs)
+print(output)
+print(output == answer)
+
+
+logs = ["1 n u", "r 527", "j 893", "6 14", "6 82"]
+answer = ["1 n u", "r 527", "j 893", "6 14", "6 82"]
+output = s.reorderLogFiles(logs)
+print(output)
+print(output == answer)
+
+logs = ["t kvr", "r 3 1", "i 403", "7 so", "t 54"]
+answer = ["t kvr", "7 so", "r 3 1", "i 403", "t 54"]
 output = s.reorderLogFiles(logs)
 print(output)
 print(output == answer)
