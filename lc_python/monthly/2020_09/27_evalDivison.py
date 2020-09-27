b59e3a59cf4bd35ca46d9db695305a304675dc98
Tony <aangus@gmail.com>
2020-09-27 02:31:11 -0700

    wip 2020 sept 27th lc daily

--- lc_python/monthly/2020_09/27_evalDivison.py
+++ lc_python/monthly/2020_09/27_evalDivison.py
@@ -17,7 +17,7 @@
 #     Output: 
 #         [6.00000,0.50000,-1.00000,1.00000,-1.00000]
 #     Explanation: 
-#         Given: a / b = 2.0, b / c = 3.0
+#         Given: a / b = 2.0, b / c = 3.0 ---- a / b = 2, b / a = 1/2
 #         queries are: a / c = ?, b / a = ?, a / e = ?, a / a = ?, x / x = ?
 #         return: [6.0, 0.5, -1.0, 1.0, -1.0 ]
 #
@@ -52,7 +52,16 @@ from typing import List
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         solutionList = []
+        variables = {}
 
+        for eqs in equations:
+            for i in eqs:
+                if i not in variables:
+                    variables[i] = 1
+        print(variables)
+        for i in range(len(equations)):
+            variables[equations[i][0]] = variables[equations[i][1]] * values[i]
+        print(variables)
         return solutionList
 
 s = Solution()
