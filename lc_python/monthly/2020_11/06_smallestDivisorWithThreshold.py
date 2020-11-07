fc3f66eee90be350af934830dc83b28c69db35bd
Tony <aangus@gmail.com>
2020-11-07 13:26:05 -0800

    finished lc daily for nov 6 - finally

--- lc_python/monthly/2020_11/06_smallestDivisorWithThreshold.py
+++ lc_python/monthly/2020_11/06_smallestDivisorWithThreshold.py
@@ -33,33 +33,80 @@ class Solution:
     def smallestDivisor(self, nums: List[int], threshold: int) -> int:
         if nums == None or len(nums) == 0 or threshold == None or threshold == 0:
             return 0
-        debug = True
-        sum = 0
+        debug = False
         divisor = 1
-        for i in nums:
-            sum += i
-
-        ##### BINARY LOW TO HIGH #####
         
+        def getSum(div):
+            s = 0
+            for i in nums:
+                v = ( int(i / div) + (i % div > 0) )
+                s += v
+            return s
+
+        ##### QUICKER Binary from Solutions #####       
+        l = 1
+        r = max(nums)
+        while l < r:
+            mid = l + (r - l) // 2
+            if getSum(mid) <= threshold:
+                r = mid
+            else:
+                l = mid + 1
+        divisor = l
+        if debug: print(divisor)
+
+        return divisor
+        
+        ##### BINARY 1 to THRESHOLD #####
+        # possibleSolutions = []
+        # def binarySearch (l, r, t): 
+        #     # s = 0
+        #     # when r and l cross we know we have full search
+        #     if r >= l: 
+        #         mid = l + (r - l) // 2
+        #         s = getSum(mid)
+        #         if debug: print(mid, s)
+
+        #         # if sum <= threshold store and search even smaller div for larger sum
+        #         if s <= t:
+        #             possibleSolutions.append(mid)
+        #             return binarySearch(l, mid - 1, t)
+        #         # needed sum needs to be smaller to need to increase divisor 
+        #         elif s > t:
+        #             return binarySearch(mid + 1, r, t)
+        #     else: # min is not possible
+        #         return 1
+        
+        # result = binarySearch(1, max(nums), threshold)
+
+        # if debug: print(result)
+
+        # possibleSolutions.sort()
+        # if debug: print(possibleSolutions)
+        # divisor = possibleSolutions[0]
+
+        # return divisor
 
         ##### LOW TO HIGH #####
         # priorSum = threshold
         # for i in range(1, sum-1):
-        #     sum = 0
+        #     numSum = 0
         #     for j in nums:
         #         val = ( int(j / i) + (j % i > 0) )
-        #         sum += val
+        #         numSum += val
         #         if debug: print(i,j,val,sum)
         #     if debug: print(sum)
-        #     if sum == threshold:
+        #     if numSum == threshold:
         #         if debug: print('matches threshold')
         #         divisor = i
         #         break
-        #     elif sum < threshold and priorSum > threshold:
+        #     elif numSum < threshold and priorSum > threshold:
         #         if debug: print('using previous')
         #         divisor = i
         #         break
         #     priorSum = sum
+        #
+        #     return divisor
         
         ##### HIGH TO LOW #####
         # startRange = 0
@@ -68,53 +115,57 @@ class Solution:
         # else:
         #     startRange = threshold
         # for i in range(startRange - 1, 0, -1):
-        #     sum = 0
+        #     numSum = 0
         #     for j in nums:
         #         val = ( int(j / i) + (j % i > 0) )
-        #         sum += val
+        #         numSum += val
         #         if debug: print(i,j,val,sum)
         #     if debug: print(sum)
-        #     if sum <= threshold:
+        #     if numSum <= threshold:
         #         if debug: print('matches threshold')
         #         divisor = i
         #         break
-        #     # elif sum > threshold and priorSum < threshold:
+        #     # elif numSum > threshold and priorSum < threshold:
         #     #     if debug: print('using previous')
         #     #     divisor = priorSum
         #     #     break
         #     priorSum = sum
-        
-        return divisor
+        #
+        #     return divisor
 
 s = Solution()
-# nums = [1,2,5,9]
-# threshold = 6
-# solution = 5
-# output = s.smallestDivisor(nums, threshold)
-# print("%s | %s" % ("PASS" if output == solution else "FAIL", output))
+nums = [1,2,5,9]
+threshold = 6
+solution = 5
+output = s.smallestDivisor(nums, threshold)
+print("%s | %s" % ("PASS" if output == solution else "FAIL", output))
 
-# nums = [2,3,5,7,11]
-# threshold = 11
-# solution = 3
-# output = s.smallestDivisor(nums, threshold)
-# print("%s | %s" % ("PASS" if output == solution else "FAIL", output))
+nums = [2,3,5,7,11]
+threshold = 11
+solution = 3
+output = s.smallestDivisor(nums, threshold)
+print("%s | %s" % ("PASS" if output == solution else "FAIL", output))
 
-# nums = [19]
-# threshold = 5
-# solution = 4
-# output = s.smallestDivisor(nums, threshold)
-# print("%s | %s" % ("PASS" if output == solution else "FAIL", output))
+nums = [19]
+threshold = 5
+solution = 4
+output = s.smallestDivisor(nums, threshold)
+print("%s | %s" % ("PASS" if output == solution else "FAIL", output))
 
-# nums = [1,2,3]
-# threshold = 1000000
-# solution = 1
-# output = s.smallestDivisor(nums, threshold)
-# print("%s | %s" % ("PASS" if output == solution else "FAIL", output))
-
-
+nums = [1,2,3]
+threshold = 1000000
+solution = 1
+output = s.smallestDivisor(nums, threshold)
+print("%s | %s" % ("PASS" if output == solution else "FAIL", output))
 
 nums = [91,41,78,86,8]
 threshold = 114
 solution = 3
+output = s.smallestDivisor(nums, threshold)
+print("%s | %s" % ("PASS" if output == solution else "FAIL", output))
+
+nums = [19]
+threshold = 5
+solution = 4
 output = s.smallestDivisor(nums, threshold)
 print("%s | %s" % ("PASS" if output == solution else "FAIL", output))
